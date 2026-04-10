481a3afe6af2fe8420eaaa2941f4769794f19054
agoncalveslx <a.goncalveslx@gmail.com>
2026-04-10 16:13:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -841,7 +841,7 @@ def desenhar_mapa_tatico(posicao, velocidade, contexto, risco):
 
     deck = pdk.Deck(
         map_provider="carto",
-        map_style=pdk.map_styles.CARTO_DARK,
+        map_style = pdk.map_styles.CARTO_DARK_MATTER,
         initial_view_state=view_state,
         layers=[
             layer_trajetoria,
