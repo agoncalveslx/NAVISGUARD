f0a8df68915d85e57c7750dd67a37366ff38d6c1
agoncalveslx <a.goncalveslx@gmail.com>
2026-04-11 08:33:25 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+
+def configurar_pagina():
+    st.set_page_config(
+        page_title="NAVISGUARD",
+        layout="wide",
+        initial_sidebar_state="collapsed"
+    )
