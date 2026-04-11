46db67361fba525906693f884e98ade01db879a1
agoncalveslx <a.goncalveslx@gmail.com>
2026-04-11 08:34:37 +0100

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,31 @@
+nomes_indicadores = {
+    "I1": "Anomalia de identidade",
+    "I2": "Alteração anormal de identidade",
+    "I3": "Plausibilidade cinemática",
+    "I4": "Consistência espaço-temporal",
+    "I5": "Consistência contextual",
+    "I6": "Consistência entre fontes"
+}
+
+siglas_indicadores = {
+    "I1": "Identidade",
+    "I2": "Alt. identidade",
+    "I3": "Cinemática",
+    "I4": "Espaço-tempo",
+    "I5": "Contexto",
+    "I6": "Entre fontes"
+}
+
+pesos = {
+    "I1": 3,
+    "I2": 2,
+    "I3": 2,
+    "I4": 2,
+    "I5": 1,
+    "I6": 3
+}
+
+opcoes_posicao = ["Normal", "Ligeiramente suspeita", "Muito suspeita"]
+opcoes_velocidade = ["Normal", "Ligeiramente suspeito", "Muito suspeito"]
+opcoes_radar = ["Concordante", "Parcialmente discordante", "Discordante"]
+opcoes_contexto = ["Normal", "Pouco habitual", "Muito suspeito"]
