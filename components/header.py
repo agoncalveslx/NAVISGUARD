8b9598a8b2ccde4f22043e08eef8184de914e290
GuilhermeFrazaoEN <frazaoguilherme@gmail.com>
2026-05-07 15:58:37 +0100

    Update header.py

--- components/header.py
+++ components/header.py
@@ -31,7 +31,7 @@ def render_header():
 """)
 
         st.markdown("""
-### Funç
+### Funções Principais
 1. **Gerar indicadores internos de risco (I1–I6)**  
    Avalia identidade, comportamento cinemático, coerência espacial e consistência entre fontes.
 
